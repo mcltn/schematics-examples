435af476186102adbaa027104d34dc85b635b847
Matt Colton <matt.colton@gmail.com>
2017-05-26 09:26:04 -0500

    Initial template

---
+++ schem-ex-1.tf
@@ -0,0 +1,20 @@
+
+provider "ibmcloud" {
+  softlayer_username = "${var.softlayer_username}"
+  softlayer_api_key = "${var.softlayer_apikey}"
+}
+
+resource "ibmcloud_infra_virtual_guest" "schem-ex-1" {
+  hostname = "ub-schem-1"
+  domain = "colton.cc"
+  os_reference_code = "UBUNTU_LATEST"
+  datacenter = "mex01"
+  cores = 1
+  memory = 1024
+  disks = [25]
+  network_speed = 100
+  local_disk = true
+  private_network_only = true,
+  hourly_billing = true,
+  tags = ["cam"]
+}
